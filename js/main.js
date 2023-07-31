0794faecd78b2d74ad8ed2e2da8f9677cb0c8897
lucasalviac <lucasalvareziaconis@gmail.com>
2023-07-31 20:12:52 -0300

    Segundo commit

--- js/main.js
+++ js/main.js
@@ -21,30 +21,17 @@ for (let i = 0; i < arregloProductos.length; i++) {
     crear_caja_instrumento(prod)
 }
 
-let carritoJson = localStorage.getItem("carrito")
-let jsonParse = JSON.parse(carritoJson)
-
-let divCarrito = document.getElementById("carrito")
-let divCarrito2 = document.createElement("div")
-divCarrito2.innerHTML = jsonParse
-divCarrito.appendChild(divCarrito2)
-/* for(let i = 0; i<jsonParse.length; i++){
-    let divCarrito = document.getElementById("carrito")
-    let divCarrito2 = document.createElement("div")
-    divCarrito2.innerHTML = jsonParse[i]
-    divCarrito.appendChild(divCarrito2)
-}
- */
-
-
-
 let carrito = new Array()
-funcionCarrito()
 
+funcionVaciarCarrito()
 
-
-
-
+window.onload = function(){
+let carritoJson = localStorage.getItem("carrito")
+if(carritoJson){
+    carrito = JSON.parse(carritoJson)
+}
+renderizarCarrito()
+}
 
 function crear_caja_instrumento(producto) {
     let divProductos = document.getElementById("cards")
@@ -57,29 +44,21 @@ function crear_caja_instrumento(producto) {
 
         let productoElegido = ""
         productoElegido = new Producto(producto.codigo, producto.marca, producto.modelo, producto.precio)
-        carrito.push(productoElegido)
-        console.log(carrito)
-        let divCarrito = document.getElementById("carrito")
-        let divCarrito2 = document.createElement("div")
-        divCarrito2.innerHTML = producto.descripcion()
-        divCarrito.appendChild(divCarrito2)
-        guardarCarrito()
+        agregarAlCarrito(productoElegido)
     })
-
-
 
     ctn.appendChild(name)
     ctn.appendChild(boton)
     divProductos.appendChild(ctn)
 }
 
-function funcionCarrito() {
+function funcionVaciarCarrito() {
     let divCarrito = document.getElementById("vaciarCarrito")
     let divCarrito2 = document.getElementById("carrito")
     let boton2 = document.createElement("button")
     boton2.innerText = "vaciar"
     boton2.addEventListener("click", () => {
-        divCarrito2.innerHTML = ""
+        divCarrito2.innerHTML = "El carrito está vacío"
         carrito = []
         guardarCarrito()
     })
@@ -87,6 +66,29 @@ function funcionCarrito() {
 }
 
 function guardarCarrito() {
-    /* let carritoJson = JSON.stringify(carrito) */
-    localStorage.setItem("carrito", (JSON.stringify(carrito)))
+    let carritoJson = JSON.stringify(carrito)
+    localStorage.setItem("carrito", carritoJson)
+}
+
+function renderizarCarrito(){
+    let divCarrito = document.getElementById("carrito")
+    divCarrito.innerHTML = ""
+
+    if (carrito.length=== 0){
+        let divCarrito2 = document.createElement("div")
+        divCarrito2.innerHTML = "El carrito está vacío"
+        divCarrito.appendChild(divCarrito2)
+    }else{
+        for(let i = 0; i<carrito.length; i++){
+        let divCarrito2 = document.createElement("div")
+        divCarrito2.innerHTML = carrito[i]
+        divCarrito.appendChild(divCarrito2)
+    }
+}
+}
+
+function agregarAlCarrito(producto){
+    carrito.push(producto)
+    guardarCarrito()
+    renderizarCarrito()
 }
